f02251b6984f2baf100ce313e29085381bc2a9b2
gaetaan <barbaria.gaetan@gmail.com>
2018-02-08 13:20:50 +0100

    Projet d'informatique appliqué : status-bar

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaetanbarbaria
+ * Date: 08/02/2018
+ * Time: 13:08
+ */
+?>
+
+
---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaetanbarbaria
+ * Date: 08/02/2018
+ * Time: 13:07
+ */
+?>
+
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <!-- Navbar content -->
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+                <img src="/assets/icon.png" width="30" height="30" class="d-inline-block align-top rounded-circle" alt="">
+                PIA
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Accueil</a>
+                    <a class="nav-item nav-link active" href="#">Catalogue</a>
+                    <a class="nav-item nav-link active" href="#">Mon Compte</a>
+                </div>
+            </div>
+        </nav>
+
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+    </nav>
+</header>
