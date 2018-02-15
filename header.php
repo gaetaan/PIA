ef7c3e801105dc7e0c7e8688fb42604097e65a73
gaetaan <barbaria.gaetan@gmail.com>
2018-02-15 14:51:05 +0100

    Projet d'informatique appliqué : header valide, demi catalogue et page de connexion non commencée

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">
-                <img src="/assets/icon.png" width="30" height="30" class="d-inline-block align-top rounded-circle" alt="">
+                <!--img src="/assets/icon.png" width="30" height="30" class="d-inline-block align-top rounded-circle" alt=""-->
                 PIA
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,10 +28,24 @@
             </div>
         </nav>
 
-        <form class="form-inline">
+        <!--form-- class="form-inline" style="width: 400px;">
             <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-        </form>
+        </form-->
+
+        <div class="pull-right">
+            <ul class="nav pull-right">
+                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, User <b class="caret"></b></a>
+                    <ul class="dropdown-menu navbar-dark bg-dark">
+                        <li><a href="#"><i class="icon-cog"></i> Preferences</a></li>
+                        <li><a href="#"><i class="icon-envelope"></i> Contact Support</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#"><i class="icon-off"></i> Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
 
     </nav>
 </header>
